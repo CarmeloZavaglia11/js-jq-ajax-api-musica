d862bfd5cebacbc75a690bbf042e50682352507a
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-08-28 14:40:23 +0200

    Complete

---
+++ main.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+	$('.select ul li').click(function(){
+
+		$('.select ul li').removeClass('selected');
+
+		$(this).addClass('selected');
+
+		$('.cd').removeClass('active');
+
+		var genre = $(this).text();
+
+		switch (genre) {
+			case 'Rock':
+				$('.cd.Rock').addClass('active');
+				break;
+				
+			case 'Pop':
+				$('.cd.Pop').addClass('active');
+				break;
+				
+			case 'Jazz':
+				$('.cd.Jazz').addClass('active');
+				break;
+				
+			case 'Metal':
+				$('.cd.Metal').addClass('active');
+				break;
+				
+			case 'All':
+				$('.cd').addClass('active');
+				break;
+		
+			default:
+				$('.cd').addClass('active');
+				break;
+		}
+
+	});
+
+	// CHIAMATA AJAX GET CD
+
+	$.ajax(
+		{
+			url: 'https://flynn.boolean.careers/exercises/api/array/music',
+			method: "GET",
+			success: function (data) {
+				var cd = data.response;
+				for (let i = 0; i < cd.length; i++) {
+					var source = document.getElementById("entry-template").innerHTML;
+	                var template = Handlebars.compile(source);
+					var context = cd[i];
+					var html = template(context);
+					$('.cds-container').append(html)					
+				}
+			},
+			error: function (errori) {
+				alert("ERRORE!: " + errori);
+			}
+		}
+	);
+
+});
