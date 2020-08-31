804bc22d98a10119f32d7ccbc7d9665ae70df1e6
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-08-31 10:11:55 +0200

    adjustments

--- main.js
+++ main.js
@@ -10,31 +10,39 @@ $(document).ready(function() {
 
 		var genre = $(this).text();
 
-		switch (genre) {
-			case 'Rock':
-				$('.cd.Rock').addClass('active');
-				break;
-				
-			case 'Pop':
-				$('.cd.Pop').addClass('active');
-				break;
-				
-			case 'Jazz':
-				$('.cd.Jazz').addClass('active');
-				break;
-				
-			case 'Metal':
-				$('.cd.Metal').addClass('active');
-				break;
-				
-			case 'All':
-				$('.cd').addClass('active');
-				break;
-		
-			default:
-				$('.cd').addClass('active');
-				break;
+		if (genre == 'All') {
+			$('.cd').addClass('active')
+		}  else {
+			$('.cd.' + genre).addClass('active');
 		}
+
+		
+
+		// switch (genre) {
+		// 	case 'Rock':
+		// 		$('.cd.Rock').addClass('active');
+		// 		break;
+				
+		// 	case 'Pop':
+		// 		$('.cd.Pop').addClass('active');
+		// 		break;
+				
+		// 	case 'Jazz':
+		// 		$('.cd.Jazz').addClass('active');
+		// 		break;
+				
+		// 	case 'Metal':
+		// 		$('.cd.Metal').addClass('active');
+		// 		break;
+				
+		// 	case 'All':
+		// 		$('.cd').addClass('active');
+		// 		break;
+		
+		// 	default:
+		// 		$('.cd').addClass('active');
+		// 		break;
+		// }
 
 	});
 
